c868cb3f0d14db507f8994c497a3986293accb95
jsanzucla <jsanz@mednet.ucla.edu>
2019-05-30 08:07:39 -0700

    Major update
    Add letter templates, make enhancements to email automation system such as: reading from letter from files, refactor code for easier readability (create new functions), create new variables to increase usability, add output report. Also update REDCAP project template with latest data elements

--- Recruitment/email_automation.py
+++ Recruitment/email_automation.py
@@ -4,140 +4,172 @@ Created on Wed May  1 09:59:12 2019
 
 @author: JSanz
 
-Purpose: Email each Primary Care Provider with their list of patients 
+Purpose: Email each Primary Care Provider with their list of patients
         included in the ACP cohort
 
 
 Additional features:
         Data destination
-            Update "Email sent to PCP (yes/no)" field in REDCAP
-            Create CSV report to upload to REDCAP
-            
+            Update "Email sent to PCP (yes/no)" field in REDCAP (5/20/19)
+            Create CSV report to upload to REDCAP (5/20/19)
         Data source:
-            Clarity
-            CSV file (REDCAP report)
+            CSV file (REDCAP report) (5/20/19)
             REDCAP
-        
-        
+            Clarity (on hold: future infrastrcuture changes might prevent this)
+        Other features
+            Integrate PCP name in the email body (done, 5/26/19)
+            Use file for email text (done, 5/29/19)
 """
-
+# ----------------------------------------------
 # import the following libraries
+# ----------------------------------------------
+import csv
 import smtplib
-from array import *
-from string import Template
 from tabulate import tabulate
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import texttable as tt
+from datetime import datetime
+from pytz import timezone
 
+# ----------------------------------------------
 # Variables
+# ----------------------------------------------
 text = ''
 html = ''
+email_count = 0
+#path_to_redcap_file = 'C:/Users/jsanz/Downloads/PCORI.csv'
+path_to_redcap_report = 'C:/Users/jsanz/Downloads/send_email_report.csv'
+# create list to hold PCPs and patient panels for each one
+patient_panel = []
 
-# Functions to reset 
+# set up patient counter
+pat_count = 1
+
+# set up a second pcp_email field to identified when the patient list changes
+# to a different provider
+prev_pcp_email = ''
+sender_email = 'UC Health Care Planning <UCHealthCarePlanning@mednet.ucla.edu>'
+# ----------------------------------------------
+# Functions
+# ----------------------------------------------
+# reset emails (plain and html)
+# Include paths to letter templates (plain and html)
 def get_text():
-    text = """
-Dear Dr. , 
-
-UCLA Health is reaching out to primary care patients with serious illness who have no advance care planning in CareConnect to encourage these patients to complete an advance directive and bring it to their primary care physician.  To evaluate the effect of this system-wide quality improvement intervention, we will survey some of these patients about advance care planning.  
-
-The patients listed below are candidates for this survey.  Please review the list and notify one of us if we should exclude any patient from being approached to participate because:
-•         the patient is cognitively incapable of being surveyed or
-•         the patient would be harmed by being surveyed or
-•         the patient does not speak English or Spanish.
-
-List of your patients
-{table}
-
-If you do not respond with patients to be excluded, we will assume that all patients can be contacted.
-
-Thank you,
-
-
-Neil Wenger                                                                            Anne Walling
-NWenger@mednet.ucla.edu                                               AWalling@mednet.ucla.edu 
-310-794-2288                                                                          310-794-0741
- 
-
-"""
+    with open('C:/Users/jsanz/Documents/GitHub/UC_Event_Care_planning/Recruitment/letter.txt', 'r') as myfile:
+        text = myfile.read()
     return text
 
 def get_html():
-    html = """
-    <html><body><p>Dear ,</p>
-    <p>We have recently identified the following patients as candidates for the ACP blah, blah, blah</p>
-    {table}
-    <p>Please, do not hesirtate to contact us if you are aware that any of these patients should not be included etcetera...</p>
-    <p>Regards,</p>
-    <p>ACP team</p>
-    </body></html>
-    """
+    with open('C:/Users/jsanz/Documents/GitHub/UC_Event_Care_planning/Recruitment/letter.html', 'r') as myfile:
+        html = myfile.read()
     return html
 
-# Set up the email parameters
-#me = 'me@gmail.com'
-#password = 'passWord'
-#server = 'smtp.gmail.com:587'
-#you = 'you@gmail.com'
-
-
-# create test data
-pcp_tup = ((12345,'drno@mednet.ucla.edu', 'Dr No'),(67890,'jsanz@mednet.ucla.edu','Dr Javier Sanz' ))
-    
-patient_tup = ((12345, '1111111', 'john' ,'doe')
-,(12345, '2222222', 'jane' ,'new')
-,(67890, '4040604','Curton' ,'Anastassia')
-,(67890, '6627934','Russell' ,'Robinett')
-,(67890, '1683497','Giddy' ,'Jaine')
-,(67890, '2819889','Shilliday' ,'Margy')
-,(67890, '5818979','Kingscott' ,'Cissy')
-,(67890, '8259685','Patton' ,'Roch')
-,(67890, '7066200','Stockton' ,'Leta')
-,(67890, '0720085','Kenningley' ,'Kaine')
-,(67890, '3260196','Schettini' ,'Marve')
-,(67890, '9288125','Frankham' ,'Christean')
-,(67890, '4951694','Saward' ,'Merilyn')
-,(67890, '4462494','Fiddy' ,'Gordy')
-,(67890, '2398769','Kondratowicz' ,'Kahaleel')
-,(67890, '8540721','Bennie' ,'Brandtr')
-)
-# find provider
-for record in pcp_tup:
-    # load provider into list
-    pcp = []
-    pcp = record
-#    reset patient panel list
-    patient_panel = []
-    print(record)
-    # add header to patient panel list
-    patient_panel.insert(0, ['mrn','first name','last name'])
-#   Look for patients for this provider
-    for pat in patient_tup:
-        # if patient pcp matches provider, add to patient_panel list
-        if pcp[0] == pat[0]:
-            print(pat)
-            count = len(patient_panel) + 1
-            patient_panel.insert(count, (pat))
-# After patient panel for provider has been completed, create message body
+# send email
+def send_email(receiver_email, receiver_name, patient_panel):
     text = get_text()
     html = get_html()
-    text = text.format(table=tabulate(patient_panel, headers="firstrow", tablefmt="grid"))
+    # Reset patient table for email
+    patient_table = []
+    # Add header
+    patient_table.append(['mrn', 'first name', 'last name'])
+    # Generate report
+    for item in patient_panel:
+        patient_table.append([str(item[1]), str(item[2]), str(item[3])])
+        # capture present time for report
+        los_angeles_time = timezone('America/Los_Angeles')
+        ts = datetime.now(los_angeles_time)
+        start_time = str(ts.strftime("%Y-%m-%d %H:%M:%S"))
+        # save patient record to report
+        email_sent_report.write(str(item[0]) + ',1,' + start_time + '\n')
+
+    # Generate plain email version
+    text = text.format(patients=tabulate(patient_table, headers="firstrow",
+                                         tablefmt="pipe"),
+                        name = receiver_name)
     print(text)
-#    html = html.format(table=tabulate(patient_panel, headers="firstrow", tablefmt="myformat"))
-#    print(html)
-# set up message parameters
-    message = MIMEMultipart( "alternative", None, [MIMEText(text)])
-                                                   #MIMEText(html,'html')])
+    # Generate html email version
+    html = html.format(patients=tabulate(patient_table, headers="firstrow",
+                                         tablefmt="html"),
+                        name = receiver_name)
+    print(html)
+
+    # set up message parameters
+    message = MIMEMultipart("alternative", None, [MIMEText(text, 'plain'),
+                                                  MIMEText(html, 'html')])
     message['Subject'] = 'UC Health Advance Care Planning Study'
-    message['From'] = 'ACP-UCLA@mednet.ucla.edu'
-    message['To'] = pcp[1]
-# set up SMTP server parameters
-    server = smtplib.SMTP('SMTP server address')
+    message['From'] = sender_email
+    message['To'] = receiver_email
+
+    # set up SMTP server parameters
+    server = smtplib.SMTP('SMTP server')
     server.ehlo()
-#    server.starttls()
-#    server.login(me, password)
-    server.sendmail('ACP-UCLA@mednet.ucla.edu', 'jsanz@mednet.ucla.edu', message.as_string())
+    server.sendmail(sender_email, receiver_email, message.as_string())
     server.quit()
-#    print(message.as_string())
+
 # Reset text and html fields
     text = get_text()
     html = get_html()
+
+# ----------------------------------------------
+# Read data file
+# ----------------------------------------------
+with open('C:/Users/jsanz/Downloads/PCORI.csv') as f:
+    reader = csv.reader(f)
+    pcp_patient_list = list(reader)
+# delete   headers
+del pcp_patient_list[0]
+
+# open file to save sent email report. It shall be changed for the second run
+email_sent_report = open('path_to_redcap_report', 'w')
+email_sent_report.write('study_id,first_email_sent_pcp_yn,first_email_sent_pcp_dt\n')
+
+
+# Loop through PCP-patient list, for every PCP, do an subloop to find all pats
+for record in pcp_patient_list:
+    # Save PCP email and PCP name to variables
+    pcp_email = str(record[3])
+    pcp_name = str(record[2])
+
+    # first record can be directly put into patient_panel list
+    if pat_count == 1:
+        # capture study_id, mrn, patient last name, potient first name
+        patient_panel.append([str(record[0]), str(record[4]), str(record[6]),
+                              str(record[5])])
+        pat_count += 1
+        prev_pcp_email = pcp_email
+        prev_pcp_name = pcp_name
+    # second patient needs to check if PCP is still the same
+    else:
+        # If PCP si still the same, add patient to patient_panel, add count
+        # record PCP email
+        if pcp_email == prev_pcp_email:
+            # capture study_id, mrn, patient last name, potient first name
+            patient_panel.append([str(record[0]), str(record[4]),
+                                  str(record[6]), str(record[5])])
+            pat_count += 1
+            prev_pcp_email = pcp_email
+            prev_pcp_name = pcp_name
+        else:
+            # If PCP has changed, send email to last PCP, reset lists, load
+            # new patient, add count, and record PCP email
+            send_email(prev_pcp_email, prev_pcp_name, patient_panel)
+            email_count += 1
+            patient_panel = []
+            # capture study_id, mrn, patient last name, potient first name
+            patient_panel.append([str(record[0]), str(record[4]),
+                                  str(record[6]), str(record[5])])
+            prev_pcp_email = pcp_email
+            prev_pcp_name = pcp_name
+            pat_count += 1
+
+# send last patient list
+send_email(pcp_email, pcp_name, patient_panel)
+email_count += 1
+
+# ----------------------------------------------
+# Print results
+# ----------------------------------------------
+print('Total number of patients: {0} Total number of emails/PCP: {1}'
+      .format(pat_count, email_count))
+email_sent_report.close()
