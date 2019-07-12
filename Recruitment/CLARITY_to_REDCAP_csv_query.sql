75c66badde57f155011a603b4b6f68d81df4a8cb
jsanzucla <jsanz@mednet.ucla.edu>
2019-07-12 07:08:19 -0700

    modify query
    REDCAP project has been updated to include multiple clinic/arm combination to capture the three different banchmarks of the study: patient selection, clinic going live, and first intervention trigger.
    Additionally, we start the distinction between the PCP at the time of patient selection, and a new field to keep the information of the most current PCP
    We also added a deadline date to email text

--- Recruitment/CLARITY_to_REDCAP_csv_query.sql
+++ Recruitment/CLARITY_to_REDCAP_csv_query.sql
@@ -3,9 +3,12 @@ SELECT DISTINCT st.study_id as "study_id"
                 ,pat.pat_mrn_id as "mrn"
                 ,pat.pat_first_name as "first_name"
 				,pat.pat_last_name as "last_name"
-                ,dep.loc_name as "clinic"
-                ,prv.provider_name as "pcp"
-                ,coh.arm as "arm"
+                ,dep.loc_name as "clinic_grey"
+                ,coh.arm as "arm_grey"                
+                ,prv.provider_name as "original_pcp"
+				--,emp.email as "pcp_email" -- At UCLA this field doesn't contain the provider's email address
+                ,case when emp2.system_login is null then '' else LOWER(emp2.system_login) || '@mednet.ucla.edu' end "pcp_email"
+
 				--if you are using Oracle, this is the statement to merge the patient's address together				
 				,CASE
                   WHEN pat.add_line_1 IS NULL 
@@ -19,17 +22,11 @@ SELECT DISTINCT st.study_id as "study_id"
                       || ', ' || pat.city 
                       || ', ' || xst.abbr
                       || ' '  || pat.zip       
-                END AS address
+                END AS "address"
 				--if you are using MSSQL, this is the statement to merge the patient's address together
-				-- concat(pat.add_line_1 , ' ', pat.add_line_2, ', ', pat.city ,  ', ' , xst.abbr , ' ' , pat.zip ) END AS address
+				-- concat(pat.add_line_1 , ' ', pat.add_line_2, ', ', pat.city ,  ', ' , xst.abbr , ' ' , pat.zip ) END AS "address"
 				
                 --,pat.email_address as "email_address"		__UCLA is going to ask patient's for their email directly
---                ,pat.add_line_1 as "address"
---                ,pat.city as "city"
---                ,xst.abbr as "state"
---                ,pat.zip as "zip_code"
---               ,pat.home_phone as "phone1"
---               ,pat.WORK_PHONE as "phone2"
                 ,cm1.OTHER_COMMUNIC_NUM as "home_phone"
                 ,cm2.OTHER_COMMUNIC_NUM as "work_phone"
                 ,cm3.OTHER_COMMUNIC_NUM as "cell_phone"
@@ -39,9 +36,6 @@ SELECT DISTINCT st.study_id as "study_id"
                 ,zla.name as "language"
                 ,zla2.name as "preferred_language"
 				,zla3.name as "preferred_lang_written_mat"
-				--,emp.email as "pcp_email" -- At UCLA this field doesn't contain the provider's email address
-                
-                ,case when emp2.system_login is null then '' else LOWER(emp2.system_login) || '@mednet.ucla.edu' end "pcp_email"
 				--UCLA specific, exclude deceased patients
                 ,CASE WHEN coh.EXCLUSION_REASON = 'patient deceased' THEN 1 ELSE 0 END "patient_dead_yn"
 				
@@ -59,12 +53,15 @@ SELECT DISTINCT st.study_id as "study_id"
 								,case when cm4.CONTACT_PRIORITY is not null then cm4.CONTACT_PRIORITY 
 										when cm4.CONTACT_PRIORITY is null then 0 end) 
 						  ) as  "Phone_priority_HCWO"
-                -------------------------
+			--the first load excludes deceased patients, the next one shall include them but FLAG them as such
+
+						  ,CASE WHEN coh.EXCLUSION_REASON = 'patient deceased' THEN coh.EXCLUSION_DATE ELSE NULL END "death_date"
+
+						  -------------------------
                 --pending
                 ------------------------------
 --               ,pat.email_address as "email_address"
 --               add a field to flag patients who became restricted after being selected for the cohort
---                ,CASE WHEN EXCLUSION_REASON = 'patient deceased' THEN EXCLUSION_DATE ELSE NULL END "death_date"
 
 FROM xdr_acp_cohort_study_id            st
 JOIN XDR_ACP_COHORT			            coh ON st.pat_id = coh.pat_id
@@ -91,11 +88,13 @@ left join XDR_ACP_COHORT_bk_05172019    prm on coh.pat_id = prm.pat_id and prm.p
 where 
     -- patients in the PCORI cohort
     coh.selected = 1
-    --exclude patients that are restricted
-    and (coh.EXCLUSION_REASON is null or coh.EXCLUSION_REASON <> 'patient restricted')
+    --exclude patients that are restricted.
+	--the first loads excludes deceased patients as well, the next load shall include them but FLAG them as such
+    and (coh.EXCLUSION_REASON is null) --or coh.EXCLUSION_REASON <> 'patient restricted')
     -- patients without an AD in the last three years, or not AD at all
     and (coh.ad_polst_all = 0 OR (current_DATe - coh.LAST_AD_POLST) > 365.25 *3)
     -- patients not already in PRIME (UCLA specific)
     and prm.pat_id is null
 order by st.study_id
 ;--4631
+
